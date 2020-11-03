2b335f88488d2be13a769dd37ba610084ded0363
Marcus Antonio <marcus@soganhei.com.br>
2020-11-03 12:38:43 -0300

    Adicionar teste no FormatDateUs

--- src/services/db.spec.ts
+++ src/services/db.spec.ts
@@ -1,19 +1,23 @@
-import database, { Storage } from './db'
-const db = database({ ...Storage() })
+import database, { IDatabase } from './db'
+
+const storagedb = {} as IDatabase
+const db = database(storagedb)
 
 describe('Database key/value', () => {
   it('Criar registro', async () => {
-    db.Create({
+    const usuario =  await db.Create({
       id: 1,
       nome: 'Marcus',
       key: 'teste',
     })
 
-    db.Create({
+    const usuario2 = await db.Create({
       id: 2,
       nome: 'Antonio',
       key: 'teste',
     })
+    expect([usuario,usuario2]).toEqual([true,true])
+
   })
 
   it('Listar registro', async () => {
@@ -49,13 +53,16 @@ describe('Database key/value', () => {
       key: 'teste',
     }
 
-    await db.Update(1, cliente)
+    const alterado = await db.Update(1, cliente)
+    expect(alterado).toBe(true)
 
     const item = await db.Findbyid(1)
     expect(item.nome).toBe('Marcus Antonio')
   })
   it('Deletar registro', async () => {
-    await db.Delete(1)
+
+    const deletado =  await db.Delete(1)
+    expect(deletado).toBe(true)
 
     const items = await db.Find('teste')
     expect(items.length).toBe(1)
